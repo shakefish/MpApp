eb0d945196255869f6828a9b3aaf04e6853a5a85
shakefish <williamozy@gmail.com>
2020-06-07 23:24:21 +0800

    Updates

---
+++ precache-manifest.79b0bdec4feebcd106c39dde6dc7bead.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2d0e9e039b6dac3312e203c5c29d35",
+    "url": "/MyApp/index.html"
+  },
+  {
+    "revision": "ee8714cedbad2caa31dd",
+    "url": "/MyApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d01d5ebf84d81e9c7aac",
+    "url": "/MyApp/static/js/2.d573bbd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyApp/static/js/2.d573bbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8714cedbad2caa31dd",
+    "url": "/MyApp/static/js/main.10e95c26.chunk.js"
+  },
+  {
+    "revision": "7f31004a2fa3ad05eae1",
+    "url": "/MyApp/static/js/runtime-main.3e0ac79e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
