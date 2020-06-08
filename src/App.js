9315e9c804241d3772de6348ee814bc54d6a6526
shakefish <williamozy@gmail.com>
2020-06-08 19:50:10 +0800

    Complete Table Body with props

--- src/App.js
+++ src/App.js
@@ -26,10 +26,28 @@ function Apps() {
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+
     return (
-      
       <div className="container">
-        <Table />
+        <Table characterData={characters} />
       </div>
     )
   }
